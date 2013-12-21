9e7af8532b396d913899d50581d4a57fae7559bc
ThisIsAreku <alexandre.boucey@gmail.com>
2013-12-21 18:53:38 +0100

    Others functions in Volatile

--- src/main/java/fr/areku/minecraft/sec/Volatile.java
+++ src/main/java/fr/areku/minecraft/sec/Volatile.java
@@ -4,12 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Volatile {
-	private static Map<String, Object> d = new HashMap<String, Object>();
-	public static Object get(String key){
-		if(!d.containsKey(key)) return null;
-		return d.get(key);
-	}
-	public static void set(String key, Object o){
-		d.put(key,  o);
-	}
+    private static Map<String, Object> d = new HashMap<String, Object>();
+
+    public static Object get(String key) {
+        if (!d.containsKey(key)) return null;
+        return d.get(key);
+    }
+
+    public static void set(String key, Object o) {
+        d.put(key, o);
+    }
+
+    public static void delete(String key) {
+        if (d.containsKey(key))
+            d.remove(key);
+    }
+
+    public static boolean contains(String key) {
+        return d.containsKey(key);
+    }
 }
