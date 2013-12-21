e686c5074a4b9ef55ac557685aea25ebc9b4438b
ThisIsAreku <alexandre.boucey@gmail.com>
2013-12-21 18:53:23 +0100

    Better Permission reload

--- src/main/java/fr/areku/minecraft/sec/PermissionReload.java
+++ src/main/java/fr/areku/minecraft/sec/PermissionReload.java
@@ -2,30 +2,25 @@ package fr.areku.minecraft.sec;
 
 import java.util.TimerTask;
 
-import org.bukkit.command.CommandException;
-
-//import ru.tehkode.permissions.bukkit.PermissionsEx;
-
 public class PermissionReload extends TimerTask {
-	//private SecurityPlugin plugin;
+    private SecurityPlugin plugin;
 
-	public PermissionReload(SecurityPlugin plugin) {
-		//this.plugin = plugin;
-	}
+    public PermissionReload(SecurityPlugin plugin) {
+        this.plugin = plugin;
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
+        if (SecurityPlugin.verbose)
+            SecurityPlugin.log("reloading permissions");
 
-		try {
-			if (SecurityPlugin.verbose)
-				SecurityPlugin.log("reloading permissions");
-			
-			//PermissionsEx.getPermissionManager().reset();
-			if (SecurityPlugin.verbose)
-					SecurityPlugin.log("\t>OK");
-		} catch (CommandException ce) {
-			SecurityPlugin.logException(ce, "Permissions reload error");
-		}
-	}
+        try {
+            ru.tehkode.permissions.bukkit.PermissionsEx.getPermissionManager().reset();
+            if (SecurityPlugin.verbose)
+                SecurityPlugin.log("\t>OK");
+        } catch (ru.tehkode.permissions.exceptions.PermissionBackendException e) {
+            //e.printStackTrace();
+        }
+    }
 
 }
